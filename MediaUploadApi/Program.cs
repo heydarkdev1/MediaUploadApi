8cff9a44336ff6b0ab6a1636ef761a4ca2476e6a
Indranil Dutta <indranil.dutta1999@hotmail.com>
2025-11-28 10:39:32 +0530

    Commit

--- MediaUploadApi/Program.cs
+++ MediaUploadApi/Program.cs
@@ -1,10 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 
+// Enable Swagger Services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+// Enable Swagger ALWAYS (Development + Production)
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Media Upload API v1");
+});
 
 // Configure the HTTP request pipeline.
 
